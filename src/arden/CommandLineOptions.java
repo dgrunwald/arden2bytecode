193ab957d5b30bde4f1269a0072db47ba03c6398
hflicka <hflicka@gmail.com>
2011-08-24 15:38:45 +0200

    added command line options

---
+++ src/arden/CommandLineOptions.java
@@ -0,0 +1,49 @@
+package arden;
+
+import java.util.List;
+
+import uk.co.flamingpenguin.jewel.cli.CommandLineInterface;
+import uk.co.flamingpenguin.jewel.cli.Option;
+import uk.co.flamingpenguin.jewel.cli.Unparsed;
+
+@CommandLineInterface(application="arden2bytecode")
+public interface CommandLineOptions {
+	@Option(shortName = "r", 
+			description = "run MLM file or already compiled MLM class file")
+	boolean getRun();
+	
+	@Option(helpRequest = true, 
+			description = "display help", 
+			shortName = {"h", "?"})
+	boolean getHelp();
+	
+	@Option(shortName = "f", 
+			description = "input files (MLM files or MLM class files)")
+	List<String> getInputFiles();
+	boolean isInputFiles();
+	
+	@Unparsed
+	List<String> getFiles();
+	boolean isFiles();
+	
+	@Option(shortName = "o",
+			description = "Output file name to compile .MLM file to. You can also specify a directory e.g. in order to compile multiple MLMs.")
+	String getOutput();
+	boolean isOutput();
+	
+	@Option(shortName = "a",
+			description = "Arguments to MLM if running a MLM.")
+	String getArguments();
+	boolean isArguments();
+	
+	/*
+	@Option(shortName = "t",
+			defaultValue = "15",
+			description = "worker thread count (default = 15)")
+	int getThreadCount();
+	
+	@Option(shortName = "l",
+			description = "log file name")
+	String getLogFile();
+	boolean isLogFile();*/	
+}
