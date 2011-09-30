ad2fe7fe20816607cf13a46e69a52ac77694506a
Hannes Flicka <hflicka@gmail.com>
2011-09-30 15:07:22 +0200

    spaces to tabs

--- src/arden/runtime/jdbc/DriverHelper.java
+++ src/arden/runtime/jdbc/DriverHelper.java
@@ -8,44 +8,44 @@ import java.util.Properties;
 
 public class DriverHelper implements Driver
 {
-    private final Driver driver;
+	private final Driver driver;
 
-    public DriverHelper(Driver driver)
-    {
-        if (driver == null)
-        {
-            throw new IllegalArgumentException("Driver must not be null.");
-        }
-        this.driver = driver;
-    }
+	public DriverHelper(Driver driver)
+	{
+		if (driver == null)
+		{
+			throw new IllegalArgumentException("Driver must not be null.");
+		}
+		this.driver = driver;
+	}
 
-    public Connection connect(String url, Properties info) throws SQLException
-    {
-       return driver.connect(url, info);
-    }
+	public Connection connect(String url, Properties info) throws SQLException
+	{
+		return driver.connect(url, info);
+	}
 
-    public boolean acceptsURL(String url) throws SQLException
-    {
-       return driver.acceptsURL(url);
-    }
+	public boolean acceptsURL(String url) throws SQLException
+	{
+		return driver.acceptsURL(url);
+	}
 
-    public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException
-    {
-        return driver.getPropertyInfo(url, info);
-    }
+	public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException
+	{
+		return driver.getPropertyInfo(url, info);
+	}
 
-    public int getMajorVersion()
-    {
-        return driver.getMajorVersion();
-    }
+	public int getMajorVersion()
+	{
+		return driver.getMajorVersion();
+	}
 
-    public int getMinorVersion()
-    {
-        return driver.getMinorVersion();
-    }
+	public int getMinorVersion()
+	{
+		return driver.getMinorVersion();
+	}
 
-    public boolean jdbcCompliant()
-    { 
-        return driver.jdbcCompliant();
-    }
+	public boolean jdbcCompliant()
+	{ 
+		return driver.jdbcCompliant();
+	}
 }
