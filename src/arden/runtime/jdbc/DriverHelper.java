152da3a78652be56e9b1ea184bde974886c7ba21
hflicka <hflicka@gmail.com>
2012-02-26 23:23:48 +0100

    changed license to GPL v3. added license headers to source files

--- src/arden/runtime/jdbc/DriverHelper.java
+++ src/arden/runtime/jdbc/DriverHelper.java
@@ -1,51 +1,75 @@
+// arden2bytecode
+// Copyright (c) 2010, Daniel Grunwald
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are
+// permitted provided that the following conditions are met:
+//
+// - Redistributions of source code must retain the above copyright notice, this list
+//   of conditions and the following disclaimer.
+//
+// - Redistributions in binary form must reproduce the above copyright notice, this list
+//   of conditions and the following disclaimer in the documentation and/or other materials
+//   provided with the distribution.
+//
+// - Neither the name of the owner nor the names of its contributors may be used to
+//   endorse or promote products derived from this software without specific prior written
+//   permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS &AS IS& AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 package arden.runtime.jdbc;
 
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverPropertyInfo;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.util.Properties;
+import java.util.logging.Logger;
 
-public class DriverHelper implements Driver
-{
+public class DriverHelper implements Driver {
 	private final Driver driver;
 
-	public DriverHelper(Driver driver)
-	{
-		if (driver == null)
-		{
+	public DriverHelper(Driver driver) {
+		if (driver == null) {
 			throw new IllegalArgumentException("Driver must not be null.");
 		}
 		this.driver = driver;
 	}
 
-	public Connection connect(String url, Properties info) throws SQLException
-	{
+	public Connection connect(String url, Properties info) throws SQLException {
 		return driver.connect(url, info);
 	}
 
-	public boolean acceptsURL(String url) throws SQLException
-	{
+	public boolean acceptsURL(String url) throws SQLException {
 		return driver.acceptsURL(url);
 	}
 
-	public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException
-	{
+	public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
 		return driver.getPropertyInfo(url, info);
 	}
 
-	public int getMajorVersion()
-	{
+	public int getMajorVersion() {
 		return driver.getMajorVersion();
 	}
 
-	public int getMinorVersion()
-	{
+	public int getMinorVersion() {
 		return driver.getMinorVersion();
 	}
 
-	public boolean jdbcCompliant()
-	{ 
+	public boolean jdbcCompliant() {
 		return driver.jdbcCompliant();
+	}
+
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		return driver.getParentLogger();
 	}
 }
